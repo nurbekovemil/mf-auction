3652e346bfe1c0807ba0ef125d77518126ac47b8
nurbekovemil <nurbekovemil@mail.ru>
2025-07-28 11:09:58 +0600

    added user change password

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../services/auth');
+const {auth} = require('../middlewares/authMiddleware');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.post('/change-password', auth, authController.changePassword);
 
 module.exports = router;
--- src/services/auth.js
+++ src/services/auth.js
@@ -3,14 +3,14 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const JWT_SECRET = process.env.JWT_SECRET;
-
+const default_password = process.env.DEFAULT_PASSWORD;
 exports.register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email } = req.body;
   try {
     const existing = await User.findOne({ where: { email } });
     if (existing) return res.status(400).json({ message: 'Пользователь с таким email уже существует' });
 
-    const hash = await bcrypt.hash(password, 10);
+    const hash = await bcrypt.hash(default_password, 10);
     const user = await User.create({ name, email, password: hash });
 
     res.status(201).json({ message: 'Регистрация прошла успешно' });
@@ -38,5 +38,33 @@ exports.login = async (req, res) => {
     res.json({ token, user: { id: user.id, name: user.name, email: user.email, role: user.role } });
   } catch (err) {
     res.status(500).json({ message: 'Ошибка при авторизации', error: err.message });
+  }
+};
+
+exports.changePassword = async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  const userId = req.user.id; // предполагаем, что user берется из JWT через middleware
+
+  try {
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'Пользователь не найден' });
+    }
+
+    // Проверка старого пароля
+    const isMatch = await bcrypt.compare(oldPassword, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Неверный старый пароль' });
+    }
+
+    // Хэшируем новый пароль
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    // Обновляем пароль
+    await user.update({ password: hashedPassword });
+
+    res.json({ message: 'Пароль успешно изменен' });
+  } catch (err) {
+    res.status(500).json({ message: 'Ошибка при смене пароля', error: err.message });
   }
 };
